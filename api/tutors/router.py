af10eb76b7b19d195e4e05620baf1ab42292089b
edo <mdinh@hawk.iit.edu>
2021-08-27 23:14:31 -0500

    feat: add get tutor profile

--- api/tutors/router.py
+++ api/tutors/router.py
@@ -15,6 +15,7 @@ from models.function import get_tutors_by_rating
 from models.function import get_tutors_by_active_status
 from models.function import get_tutors_by_first_name
 from models.function import get_tutors_by_last_name
+from models.function import get_tutor_by_user
 
 tutor_blueprint = Blueprint("tutors", __name__, url_prefix="/tutors")
 
@@ -48,3 +49,9 @@ def search_tutors():
         return jsonify(get_tutors_by_last_name(last_name))
 
     return {}
+
+
+@tutor_blueprint.route("/<string:username>", methods=["GET"], strict_slashes=False)
+@api()
+def get_tutor_profile(username):
+    return get_tutor_by_user(username)
